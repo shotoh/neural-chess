e775cfe7a5794a9faa566c7ef8c7ba9d3cbc01c0
shotoh <trueshotoh@gmail.com>
2024-02-26 11:19:45 -0800

    started to implement fix for tensors

--- train.py
+++ train.py
@@ -4,15 +4,14 @@ from keras.src.callbacks import ModelCheckpoint
 from utils import *
 
 
-def get_random_move(games):
-    random_game = games[np.random.randint(0, len(games))]
-    moves = random_game.split(' ')
+def get_random_move(game_tensor):
+    moves = tf.strings.split(game_tensor, sep=' ')
     if len(moves) < 3:
         return None
     random_index = np.random.randint(0, len(moves) - 2)
     chess_board = chess.Board()
-    for move in moves[:random_index]:
-        chess_board.push_uci(move)
+    for i in range(random_index):
+        chess_board.push_uci(moves[i].numpy().decode())
     if random_index % 2 == 0:
         color = 'w'
     else:
@@ -22,27 +21,25 @@ def get_random_move(games):
     return x, y
 
 
-def create_dataset(games):
-    ds = tf.data.Dataset.from_tensor_slices(games)
+def create_dataset():
+    ds = tf.data.TextLineDataset('resources/output.txt')
     ds = ds.batch(10240).map(get_random_move).filter(lambda x: x is not None).unbatch()
     return ds
 
 
 if __name__ == '__main__':
     model = create_model()
-    train_data = []
-    train_labels = []
-    with open('resources/output.txt', 'r') as f:
-        lines = f.read().splitlines()
-    packed_ds = create_dataset(lines)
+    print('Creating dataset...')
+    packed_ds = create_dataset()
     print(len(packed_ds))
-    train_ds = packed_ds.skip(1024).take(51200).cache().shuffle(10240).repeat().batch(2048)
+    packed_ds = packed_ds.skip(1024).take(51200).cache()
+    train_ds = packed_ds.shuffle(10240).repeat().batch(2048)
     checkpoint_filepath = 'checkpoints/'
     model_checkpointing_callback = ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_best_only=True,
     )
-    model.fit(train_data, train_labels, batch_size=2048, epochs=100,
+    model.fit(train_ds, batch_size=2048, epochs=100,
               callbacks=[callbacks.ReduceLROnPlateau(monitor='loss', patience=10),
                          callbacks.EarlyStopping(monitor='loss', patience=15, min_delta=0.001),
                          model_checkpointing_callback])
