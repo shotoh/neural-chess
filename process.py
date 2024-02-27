6529be56db400bac38f5d2c999a19b0ba950c5b4
shotoh <trueshotoh@gmail.com>
2024-02-26 23:21:28 -0800

    parsed json line by line

--- process.py
+++ process.py
@@ -5,16 +5,17 @@ def process_pgn(pgn_file, output_file):
     print('Processing started')
     input_stream = open(pgn_file, 'r')
     output_stream = open(output_file, 'w')
-    index = 0
-    while True:
-        position = json.load(input_stream)
-        if position is None:
-            break
-        line = position['fen'] + ',' + position['evals'][0]['pvs'][0]['cp']
-        output_stream.write(line + '\n')
-        index += 1
-        if index % 1000000 == 0:
-            print(f'Processed {index} million positions')
+    num = 0
+    for line in input_stream:
+        position = json.loads(line)
+        try:
+            score = position["evals"][0]["pvs"][0]["cp"]
+        except KeyError:
+            continue
+        output_stream.write(f'{position["fen"]},{score}\n')
+        num += 1
+    if num % 1000000 == 0:
+        print(f'Processed {num} million positions')
     print('Processing finished')
 
 
