0710157d64e3373ffa81f40001791f99b62c2174
shotoh <trueshotoh@gmail.com>
2024-02-27 20:21:43 -0800

    migrated to pytorch lightning

--- process.py
+++ process.py
@@ -8,14 +8,21 @@ def process_pgn(pgn_file, output_file):
     num = 0
     for line in input_stream:
         position = json.loads(line)
+        fen = position['fen'].split(' ')[0]
         try:
-            score = position["evals"][0]["pvs"][0]["cp"]
+            score = position['evals'][0]['pvs'][0]['cp'] / 100
         except KeyError:
-            continue
-        output_stream.write(f'{position["fen"]},{score}\n')
+            mate = position['evals'][0]['pvs'][0]['mate']
+            if mate > 0:
+                score = 15
+            else:
+                score = -15
+        score = min(score, 15)
+        score = max(score, -15)
+        output_stream.write(f'{fen},{score}\n')
         num += 1
-    if num % 1000000 == 0:
-        print(f'Processed {num} million positions')
+        if num % 1000000 == 0:
+            print(f'Processed {num} million positions')
     print('Processing finished')
 
 
--- utils.py
+++ utils.py
@@ -1,52 +1,17 @@
-import re
-
+import chess
 import numpy as np
 
-letter_to_number = {
-    'a': 0,
-    'b': 1,
-    'c': 2,
-    'd': 3,
-    'e': 4,
-    'f': 5,
-    'g': 6,
-    'h': 7
-}
-
-number_to_letter = {
-    0: 'a',
-    1: 'b',
-    2: 'c',
-    3: 'd',
-    4: 'e',
-    5: 'f',
-    6: 'g',
-    7: 'h'
-}
+colors = [chess.WHITE, chess.BLACK]
+pieces = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING]
 
 
 def format_board(board):
-    layers = []
-    pieces = ['p', 'r', 'n', 'b', 'q', 'k']
-    board = board.copy()
-    for piece in pieces:
-        layers.append(format_layer(board, 'w', piece))
-    for piece in pieces:
-        layers.append(format_layer(board, 'b', piece))
-    return np.stack(np.float32(layers))
-
-
-def format_layer(board, color, piece):
-    if color == 'w':
-        piece = piece.upper()
-    s = str(board)
-    s = re.sub(f'[^{piece} \n]', '.', s)
-    s = re.sub(f'\.', '0', s)
-    s = re.sub(f'{piece}', '1', s)
-    layer = []
-    for row in s.split('\n'):
-        row = row.split(' ')
-        row = [int(x) for x in row]
-        layer.append(row)
-    return np.array(layer)
+    vectors = []
+    for color in colors:
+        for piece in pieces:
+            v = np.zeros(64)
+            for i in list(board.pieces(piece, color)):
+                v[i] = 1
+            vectors.append(v)
+    return np.float32(np.concatenate(vectors))
 
